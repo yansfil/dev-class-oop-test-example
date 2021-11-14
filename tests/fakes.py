e3e6bbffb08b9ba4f9bfbe838f556d385b5381a7
socar-grab <grab@socar.kr>
2021-11-14 23:54:38 +0900

    feat: user_service test를 작성한다

---
+++ tests/fakes.py
@@ -0,0 +1,18 @@
+from app.application.interfaces.user_repository import AbstractRepository
+from app.domain.user import User
+
+
+class FakeUserRepository(AbstractRepository):
+    def __init__(self):
+        self.users = []
+
+    def create(self, model: User):
+        self.users.append(model)
+        return model
+
+    def find_one(self, model: User):
+        for _user in self.users:
+            if _user.name == model.name:
+                return model
+        return None
+
---
+++ tests/test_service.py
@@ -0,0 +1,29 @@
+import pytest
+
+from app.application.services.user import UserService
+from app.domain.user import User
+from tests.fakes import FakeUserRepository
+
+@pytest.fixture
+def user_service():
+    repository = FakeUserRepository()
+
+    user_service = UserService(user_repository=repository)
+    return user_service
+
+def test_create_user_well(user_service):
+    user_name = "grab"
+
+    user = user_service.create_user(user_name=user_name)
+
+    assert user == User(name=user_name)
+
+
+def test_create_user_duplicated(user_service):
+    user_name = "grab"
+
+    user_service.create_user(user_name=user_name)
+
+    # 중복 가입
+    with pytest.raises(ValueError):
+        user_service.create_user(user_name=user_name)
